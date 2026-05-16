f5f3fdfcb2c964fd2e8c228325703c992b2e3de0
Eemolfinov1 <estu75121027@institutobolivar.edu.pe>
2026-05-15 21:41:34 -0500

    Ejercicios

--- clase01.dart
+++ clase01.dart
@@ -1,7 +1,34 @@
-void main(){
+void main() {
+  int edad = 50;
+
+  int edad2 = 30;
+
+  const edad3 = 40;
+
+  print('Original values: ');
+  print('Edad: $edad');
+  print('Edad 2: $edad2');
+
+  edad = 22;
   
-  String mensaje = 'Hola,mi edad es: ';
-  int edad = 27;
+  edad3==17;
+
+  print('\nUpdated values');
+  print('Edad : $edad');
+  print('Edad 3: $edad3');
+
+  List<int> edades = [20, 10, 15];
+  print(edades);
+
+  print(edades[2]);
   
-  print('$mensaje $edad');
+  
+  Map<String, dynamic> mascotas ={
+    'Perro' : 'Boby',
+    'Gato' : 'Paco', 
+    'Conejo' : 'Yunque',
+  };
+  
+  print(mascotas);
+  print(mascotas['Perro']);
 }
---
+++ clase01pract.dart
@@ -0,0 +1,13 @@
+void main() {
+  var val1 = 10;
+  var val2 = 20;
+  var resul = val1 + val2;
+  print('La suma de $val1 y de $val2');
+  print(resul);
+
+  int lado = 5;
+
+  int formu = lado * lado;
+
+  print('El área de un cuadrado será: $formu');
+}
